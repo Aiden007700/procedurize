9f2c50f27cceb2bbe65bc0d211462df5d01540b7
aiden <campbell.e.aiden@gmail.com>
2020-06-17 01:04:39 +0200

    morgan

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const options = new DocumentBuilder()
     .setTitle('Procedurize')
     .setDescription('The Procedurize API description')
@@ -13,8 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-
-
+  app.use(morgan('dev'))
   await app.listen(3000);
 }
 bootstrap();
