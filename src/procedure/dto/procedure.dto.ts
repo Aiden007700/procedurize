8a9d36cef6aa3c2022e0f9dc17d47d9f9b79ee4b
aiden <campbell.e.aiden@gmail.com>
2020-06-17 20:44:16 +0200

    add dto validation

--- src/procedure/dto/procedure.dto.ts
+++ src/procedure/dto/procedure.dto.ts
@@ -1,17 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsOptional, IsString, Min } from 'class-validator';
 
 export class ProcedureDto {
 
+  @IsInt()
   @ApiProperty()
   projectId: number
+
   @ApiProperty()
+  @IsString()
   name: string
+
   @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
   description?: string
+
   @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
   observation?: string
+
   @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
   hypothesis?: string
+
   @ApiProperty()
+  @IsInt()
+  @Min(0)
   orderSequence: number
 }
--- src/projects/dto/project.dto.ts
+++ src/projects/dto/project.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
 export class ProjectDto {
   @ApiProperty()
+  @IsString()
   name: string
+
+  @IsString()
   @ApiProperty({ required: false })
+  @IsOptional()
   description?: string
 }
